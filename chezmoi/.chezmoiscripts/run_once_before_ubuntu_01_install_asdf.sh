28a1ccf837c1f84ff4d1c9f56f2f18d325d31f68
Jung Joon Yoon <libero422@gmail.com>
2024-07-11 07:15:34 +0900

    feat: rollback to checking asdf

--- chezmoi/.chezmoiscripts/run_once_before_ubuntu_01_install_asdf.sh
+++ chezmoi/.chezmoiscripts/run_once_before_ubuntu_01_install_asdf.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-source $HOME/.asdf/asdf.sh
+# Install/upgrade asdf
+if [[ ! $(command -v asdf) ]]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+  source $HOME/.asdf/asdf.sh
+else
+  asdf update
+fi
