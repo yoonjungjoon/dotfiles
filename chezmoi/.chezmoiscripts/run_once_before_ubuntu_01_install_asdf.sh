e182959b28c23d812085c6fcfa0d1500e9bd1b27
Jung Joon Yoon <libero422@gmail.com>
2024-07-11 06:47:50 +0900

    feat: further deleted codes

--- chezmoi/.chezmoiscripts/run_once_before_ubuntu_01_install_asdf.sh
+++ chezmoi/.chezmoiscripts/run_once_before_ubuntu_01_install_asdf.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Install/upgrade asdf
-if [[ ! $(command -v asdf) ]]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-  source $HOME/.asdf/asdf.sh
-else
-  asdf update
-fi
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+source $HOME/.asdf/asdf.sh
